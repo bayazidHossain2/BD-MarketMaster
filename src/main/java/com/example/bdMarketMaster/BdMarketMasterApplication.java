640e8ee2193c53950460a1b62cb0d2923230b236
bayazidHossain2 <bh.190140@s.pust.ac.bd>
2024-07-12 01:35:39 +0600

    initial commit

---
+++ src/main/java/com/example/bdMarketMaster/BdMarketMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.bdMarketMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdMarketMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdMarketMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bdMarketMaster/BdMarketMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bdMarketMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdMarketMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
