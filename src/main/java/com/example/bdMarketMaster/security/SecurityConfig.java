8ecb7b83d052294071afe13fd00e9d370def2941
bayazidHossain2 <bh.190140@s.pust.ac.bd>
2024-07-12 12:44:16 +0600

    Jwt security configure

---
+++ src/main/java/com/example/bdMarketMaster/security/SecurityConfig.java
@@ -0,0 +1,106 @@
+package com.example.bdMarketMaster.security;
+
+import com.example.bdMarketMaster.security.jwt.AuthEntryPointJwt;
+import com.example.bdMarketMaster.security.jwt.AuthTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return  new AuthTokenFilter();
+    }
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, DefaultAuthenticationEventPublisher authenticationEventPublisher) throws Exception {
+        http.authorizeRequests(authorizeRequests -> authorizeRequests.requestMatchers("/auth/**")
+                .permitAll().anyRequest().authenticated());
+//        http.authorizeHttpRequests((requests) -> requests
+//                .requestMatchers("/auth/**").permitAll()
+//                .anyRequest().authenticated());
+        http.sessionManagement(session -> session.
+                sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler));
+        //        http.formLogin(withDefaults());
+//        http.httpBasic(withDefaults());
+//        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+                .password(passwordEncoder().encode("asdfgh"))
+                .roles("USER")
+                .build();// noop ensure password encoding
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("asdfgh1"))
+                .roles("ADMIN")
+                .build();
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+//        userDetailsManager.createUser(user1);
+//        userDetailsManager.createUser(admin);
+
+        try {
+            if (!userDetailsManager.userExists(user1.getUsername())) {
+                userDetailsManager.createUser(user1);
+            }
+            if (!userDetailsManager.userExists(admin.getUsername())) {
+                userDetailsManager.createUser(admin);
+            }
+        } catch (DataIntegrityViolationException e) {
+            // Handle exception if any
+        }
+        return userDetailsManager;
+//        return new InMemoryUserDetailsManager(user1, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
+
